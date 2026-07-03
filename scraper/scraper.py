55e5b53b75146e87c275efcc78e681e53440e721
divrajgit <145946859+divrajgit@users.noreply.github.com>
2026-07-03 20:30:58 +1000

    Import site data functions from utils

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,3 +3,9 @@ from parsers.thebodyshop import parse as parse_thebodyshop
 PARSERS = {
     "thebodyshop": parse_thebodyshop,
 }
+from utils.yaml_loader import load_sites
+from utils.json_store import (
+    load_site_data,
+    save_site_data,
+    save_all_sites
+)
